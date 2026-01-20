695f7049fbb5e1f07413742c267c5dbfdd9b00db
karolmarkut <karolmarkut11@gmail.com>
2026-01-20 19:57:26 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 class Distance:
     # Write your code here
-    pass
+        def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+
+            if not 1 <= comfort_class < 8:
+                raise ValueError
+            self.comfort_class = comfort_class
+            if not 1 <= clean_mark < 11:
+                raise ValueError
+            self.clean_mark = clean_mark
+            self.brand = brand
+
+    class CarWashStation:
+
+        def __init__(self, distance_from_city_center: int,
+                     clean_power: int, average_rating: int, count_of_rating: int):
+            self.distance_from_city_center = distance_from_city_center
+            self.clean_power = clean_power
+            self.average_rating = average_rating
+            self.count_of_rating = count_of_rating
+
+        def calculate_washing_price(self, car):
+            price = ((car.comfort_class
+                      * (self.clean_power - self.car.clean_mark)
+                      * self.average_rating)
+                     / self.distance_from_city_center)
+            return round(price, 1)
